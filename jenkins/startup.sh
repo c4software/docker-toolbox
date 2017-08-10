39ceec92e63ae0a10872acf5b8094fd3a7273381
Valentin Brosseau <c4software@gmail.com>
2017-08-10 22:09:28 +0200

    Update HOME directory

--- jenkins/startup.sh
+++ jenkins/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-$DATADIRECTORY="~/jenkins_home/"
+DATADIRECTORY="$HOME/jenkins_home/"
 
 if [ ! -d "$DATADIRECTORY" ]; then
-  mkdir $DATADIRECTORY
-  chown 1000:1000 $DATADIRECTORY
+  mkdir "$DATADIRECTORY"
+  sudo chown 1000:1000 "$DATADIRECTORY"
 fi
 
 docker run -d -p 8080:8080 -p 50000:50000 -v $DATADIRECTORY:/var/jenkins_home jenkins/jenkins:lts
